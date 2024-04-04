e9b0ee3898ea0049c89a375c3c564afc1a4ffd5f
msmith368 <94403504+msmith368@users.noreply.github.com>
2024-04-04 17:20:22 -0400

    added additional packages + began backend setup for the login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,26 +1,53 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from "./home";
 import { CgProfile } from "react-icons/cg";
 import { FaRegUser } from "react-icons/fa";
 import { RiLockPasswordLine } from "react-icons/ri";
+import React, { useState, useEffect } from "react";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 import './css/login.css'
 
 
 export default function LoginPage() {
+    const [values, setValues] = useState ({
+        username: '',
+        password: ''
+    })
+
+    const [errors, setErrors] = useState({})
+
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}));
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(validation(values));
+
+    }
+
+    function validation(values){
+        
+    }
 
     return (
         <div className = "page">
         <div className="cover">
             <h1>Login</h1>
+            <form className="loginTitle" action="" onSubmit={handleSubmit}>
             <p className="logintitle">User Name</p>
-            <input className = "logininput" type="logintext" placeholder="type your username"/>
+            <input className = "logininput" type="logintext" placeholder="type your username"
+            onChange={handleInput} name="username"/>
             <p className="logintitle">Password</p>
-            <input className = "logininput" type="password" placeholder="type your password" />
+            <input className = "logininput" type="password" placeholder="type your password" 
+            onChange={handleInput} name="password"/>
+            </form>
             <p className="pwtext">Forgot Password?</p>
-            <div className="login-btn">Login</div>
+
+            <button type='submit' className="login-btn">Login</button>
+
             <p className="logintext">Don't Have an Account?</p>
-            <a href="signup" className="logintext">SIGN UP</a>
-            <a href="home" className="logintext">Go to home page (DEVTOOL)</a>
+            <Link to="/signup" className="logintext">SIGN UP</Link>
+            <Link to="/home" className="logintext">Go to home page (DEVTOOL)</Link>
         </div>
         </div>
     );
